bdaf0cb9ca9fb8bd1ce2b8e70ddda19890915dc7
Steve Daniel <sgd3@cornell.edu>
2020-12-16 20:00:28 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -3,9 +3,11 @@ def count_strings(array)
 array.count do |element|
   element.is_a? String
 end
+end
 
 def count_empty_strings(array)
 array.delete_if { |obj| !(obj.is_a? String) }
 array.count do |element|
   element.empty?
+end
 end
